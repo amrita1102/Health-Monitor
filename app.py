c818ba2eab5c53492c8d4ce10664f5afbcd1b534
Amrita Anand <amritaanand55@gmail.com>
2024-11-12 22:03:54 +0530

    port change

--- app.py
+++ app.py
@@ -409,4 +409,4 @@ def update_charts(selected_names, table_data):
 
 # Run the app
 if __name__ == '__main__':
-    app.run_server(debug=True, port= 8050)
+    app.run_server(debug=True, port= 11011)
